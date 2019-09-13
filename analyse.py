da39e42c4c6f4a275002f55a83319c5983560b49
PiotrAbramski <piotr@cormo.pl>
2019-09-13 14:34:16 +0200

    final touches

--- analyse.py
+++ analyse.py
@@ -5,27 +5,16 @@ from pandas.core.frame import DataFrame
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-CSV = '/home/knot/Pobrane/polish_sentiment_dataset.csv'
-
 POSITIVE = 1.0
-NEUTRAL = 0
+NEUTRAL  = 0
 NEGATIVE = -1.0
 
-def save_or_display(func):
-    def handle_output():
-        func()
-        if argv[2] == '-s':
-            plt.savefig('feedback_bar.png')
-        else:
-            plt.show()
-    return handle_output()
 
-
-#@save_or_display
 def feedback_bar(df):
     """
     Displays or saves bar chart for feedback type count.
     :param df:DataFrame
+    :return bool
     """
     labels = ['Positive', 'Neutral', 'Negative']
     counts = [len(df[df.rate == POSITIVE]),
@@ -47,21 +36,32 @@ def feedback_bar(df):
     for i in range(3):
         plt.text(x=i-0.06, y=counts[i], size=10, s=counts[i])
 
-    plt.savefig('feedback_bar.png')
+    if argv[2] == '-s':
+        plt.savefig('feedback_bar.png')
+        return True
+    elif argv[2] == '-d':
+        plt.show()
+        return False
 
 
-
-#@save_or_display
 def review_histogram(df):
     """
     Displays or saves histogram for description lengths.
     :param df:
+    :return bool
     """
-    df['desc_len'] = df.apply(lambda row: len(row.description), axis=1)
-    df.hist(column='desc_len')
-    plt.savefig('histogram.png')
-    #TODO!
+    df.hist(column='length', grid=True, bins=8000, cumulative=False)
+    plt.title('Histogram of descriptions length')
+    plt.axis([20, 500, 0, 15000])
+    plt.ylabel('Count')
+    plt.xlabel('Length')
 
+    if argv[2] == '-s':
+        plt.savefig('length_histogram.png')
+        return True
+    elif argv[2] == '-d':
+        plt.show()
+        return False
 
 def filter(df, length_gte=None, rates=None) -> DataFrame:
     """
@@ -82,7 +82,14 @@ def filter(df, length_gte=None, rates=None) -> DataFrame:
     return df
 
 
-def no_correlation(df):
+def no_correlation(df) -> DataFrame:
+    """
+    Counts 'nie' word occurrence in descriptions.
+    Adds result to the no_count column and returns
+    correlation matrix.
+    :param df:DataFrame
+    :return:DataFrame correlation matrix
+    """
     df['no_count'] = 0
 
     for i, row in df.iterrows():
@@ -97,30 +104,38 @@ def no_correlation(df):
 
 if __name__ == '__main__':
     #Reading data from .csv and putting it into pandas DataFrame.
-    df = read_csv(CSV)
+    df = read_csv(argv[1])
     #Cleaning DataFrame, dropping rows with NaN
     df = df.dropna()
 
     #Task 1) Feedback visualization
-    feedback_bar(df)
+    print("1) Feedback visualization:")
+    if feedback_bar(df):
+        print('Feedback chart saved as feedback_bar.png')
+    else:
+        print('Feedback chart displayed.')
 
     #Task 2) Histogram for description lengths
-    #review_histogram(df)
+    print("\n2) Length histogram")
+    if review_histogram(df):
+        print('Length histogram saved as length_histogram.png')
+    else:
+        print("Histogram displayed.")
 
     #Task 3) Positive and negative feedback filtering
     filtered_df = filter(df, 100, [POSITIVE, NEGATIVE])
     no_neutral = NEUTRAL not in filtered_df.rate
-    len_gte100 = min(filtered_df.length) > 99
+    len_gte_100 = min(filtered_df.length) > 99
     assert no_neutral
-    assert len_gte100
-    print(f"3) Filtering DataFrame (length > 100 and rate in [POSITIVE, NEGATIVE]")
+    assert len_gte_100
+    print(f"\n3) Filtering DataFrame (length > 100 and rate in [POSITIVE, NEGATIVE]")
     print(f"Neutral not in df.rate: {no_neutral}")
-    print(f"Minimal length of description > 99: {len_gte100}\n")
+    print(f"Minimal length of description > 99: {len_gte_100}")
 
     #Task 4) Finding correlation between length and rate.
     corr = df.corr(method='pearson')
-    print(f"4) Length and rate correlation\n\n {corr}")
+    print(f"\n4) Length and rate correlation\n {corr}")
 
-    #Task 5) Interesting relation between features.
+    #Task 5) Interesting relation between features ('Nie' word usage to rate type).
     no_corr = no_correlation(df)
-    print(f"5) Number of 'nie' word used and rate correlation\n\n {no_corr}")
+    print(f"\n5) Number of 'nie' word used and rate correlation\n {no_corr}")
